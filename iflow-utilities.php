2e57ea41a0c7faddb0b73a3a6d1068b37db5c593
Marcos <macr1408@hotmail.com>
2018-06-01 17:42:02 -0300

    Fixed bugs and improved tracking shortcode

--- iflow-utilities.php
+++ iflow-utilities.php
@@ -13,49 +13,81 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_shortcode ( 'rastreo_iflow' , 'woo_iflow_rastreo_iflow_func' );
 function woo_iflow_rastreo_iflow_func( $atts, $content= NULL) {
 	if($_POST['id']){
+		$iflow_id = filter_var ($_POST['id'], FILTER_SANITIZE_SPECIAL_CHARS);		
 		ob_start();
 
+		// Entorno prod
 		$body = array(
-			'_username' => get_option('iflow_settings_username_test'),
-			'_password' => get_option('iflow_settings_password_test')
+			'_username' => get_option('iflow_settings_username_prod'),
+			'_password' => get_option('iflow_settings_password_prod')
 		);
+		$url_token = 'https://api.iflow21.com/api/login';
+		$url_tracking = 'https://api.iflow21.com/api/order/state/'.$iflow_id;	
+
+		// Entorno prueba
+		if($body['_username'] == ''){
+			$body = array(
+				'_username' => get_option('iflow_settings_username_test'),
+				'_password' => get_option('iflow_settings_password_test')
+			);
+			$url_token = 'https://test-api.iflow21.com/api/login';
+			$url_tracking = 'https://test-api.iflow21.com/api/order/state/'.$iflow_id;	
+		}
+
+		// Obtencion de token
 		$headers = array(
 			'content-type' => 'application/json'		
 		);
-		$url = 'https://test-api.iflow21.com/api/login';
-		$token = wp_remote_post($url, array(
+		$token = wp_remote_post($url_token, array(
 			'headers' => $headers,
 			'body' => json_encode($body)
 		));
 		if(is_wp_error($token)){
 			$log = new WC_Logger();		
 			$log->add( 'iflow', "Error al obtener token para rastrear: ".$token->get_error_message());
+			wc_print_notice( __( 'Hubo un error comunicándonos con el envío', 'woocommerce' ), 'error' );
+			return;
 		}
 		$token = json_decode($token['body']);
 		if($token->message === ''){
 			$token = $token->token;
 		}
-		$iflow_id = filter_var ($_POST['id'], FILTER_SANITIZE_SPECIAL_CHARS);
-		$url = 'https://test-api.iflow21.com/api/order/state/'.$iflow_id;
-		$envio = wp_remote_get($url, array(
+				
+
+		// Rastreo
+		$envio = wp_remote_get($url_tracking, array(
 			'headers' => array(
 				'Content-Type' => 'application/json',
 				'Authorization' => "Bearer ".$token
 			)
 		));
+				
+		if(is_wp_error($envio)){
+			wc_print_notice( __( 'Hubo un error comunicándonos con el envío', 'woocommerce' ), 'error' );
+			return;
+		}
 		$envio = json_decode($envio['body'],true);
-		highlight_string("<?php\n\$data =\n" . var_export($envio, true) . ";\n?>");
-		if(isset($envio['results'])){
+
+		// Impresion de datos
+		if(isset($envio['results']) && $envio['results'] !== false){
 			echo '<h3>Envío Nro: '.$envio['results']['tracking_id'].'</h3>';
 			echo "<table>";
 			echo "<tr>";
-				echo "<td>Estado actual: </td>";
-				echo "<td>".$envio['results']['shippings']['state']['state_name']."</td>";
+				echo "<th width=\"30%\">Fecha</th>";
+				echo "<th width=\"70%\">Estado actual</th>";
 			echo "</tr>";
-			echo "<tr>";
-				echo "<td>Fecha: </td>";
-				echo "<td>".$envio['results']['shippings']['state']['state_date']."</td>";
-			echo "</tr>";
+			$envios = $envio['results']['shippings'][0]['states'];
+			usort($envios, function($a, $b) {
+				$date1 = DateTime::createFromFormat('d/m/Y H:i', $a['state_date']);
+				$date2 = DateTime::createFromFormat('d/m/Y H:i', $b['state_date']);
+				return $date1 > $date2;
+			});
+			foreach ($envios as $estado_envio) {
+				echo "<tr>";
+					echo "<td>".$estado_envio['state_date']."</td>";
+					echo "<td>".($estado_envio['details'] ? $estado_envio['details'] : $estado_envio['state_name'])." </td>";
+				echo "</tr>";
+			}
 			echo "</table>";
 		}else{
 			wc_print_notice( __( 'Hubo un error, por favor intenta nuevamente', 'woocommerce' ), 'error' );
@@ -94,7 +126,7 @@ function woo_iflow_agregar_boton_etiqueta_iflow( $actions, $order ) {
 // Icono para el botón de etiquetas dentro de manage orders
 add_action( 'admin_head', 'woo_iflow_boton_etiquetas_iflow_css' );
 function woo_iflow_boton_etiquetas_iflow_css() {
-    echo '<style>.view.eti_iflow::after { font-family: woocommerce; content: "\e028" !important; }</style>';
+    echo '<style>.view.eti_iflow::after { font-family: "WooCommerce"; content: "\e028" !important; }</style>';
 }
 
 
@@ -127,7 +159,7 @@ function woo_iflow_agregar_contenido_columna_iflow( $column ) {
     global $post;
     if ( 'rastreo_iflow' === $column ) {
 		$order = wc_get_order( $post->ID );
-		if($order->get_meta('ordenretiro_iflow') !== ''){
+		if($order->get_meta('numeroenvio_iflow') !== ''){
 			echo $order->get_meta('numeroenvio_iflow');
 		}
     }
